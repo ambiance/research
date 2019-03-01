70f3c0af8299cae0a885f26c423e117d9c3e9efc
Scott Roberts <s.e.roberts0@gmail.com>
2019-02-28 22:09:57 -0800

    Add Sample Heirarchy

---
+++ sample-heirarchy/src/main.js
@@ -0,0 +1,3 @@
+import { LocationCard } from './components/location-card/index.js';
+import { PageHeader } from './components/page-header/index.js';
+import { Modal } from './components/modal/index.js';
