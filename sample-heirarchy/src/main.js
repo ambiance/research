9e128c72c65afb746374da2aa6b5f7e78fd7ccf4
Scott Roberts <s.e.roberts0@gmail.com>
2019-04-13 17:44:44 -0700

    Update Documentation

--- sample-heirarchy/src/main.js
+++
@@ -1,3 +0,0 @@
-import { LocationCard } from './components/location-card/index.js';
-import { PageHeader } from './components/page-header/index.js';
-import { Modal } from './components/modal/index.js';
